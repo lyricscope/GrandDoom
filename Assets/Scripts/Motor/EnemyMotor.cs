a4576ac13be61f41e71f4d81dc1fed2d747a513b
Suspecta12 <Toiymbetov_M7042@kst.nis.edu.kz>
2025-05-24 15:46:54 +0500

    simple enemy follow

---
+++ Assets/Scripts/Motor/EnemyMotor.cs
@@ -0,0 +1,17 @@
+﻿using Epitome;
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMotor : BaseMotor
+{
+    public NavMeshAgent enemy;
+    public GameObject target;
+    public UnityEngine.Vector3 offset = new UnityEngine.Vector3(0, 0, 0);
+    protected override void UpdateMotor()
+        {
+            enemy.SetDestination(target.transform.position + offset);
+        }
+}
